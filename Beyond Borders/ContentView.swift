afe749ea4172ae910a9493d0c41d12b52b5825a2
Davide Bellobuono <davide23bellobuono@gmail.com>
2025-03-14 17:49:47 +0100

    translate

--- Beyond Borders/ContentView.swift
+++ Beyond Borders/ContentView.swift
@@ -29,7 +29,7 @@ struct ContentView: View {
                     Text("\nWelcome in Beyond Borders")
                         .font(.largeTitle)
                         .multilineTextAlignment(.center)
-                    
+                        .foregroundColor(Color(.colorWritten))
                         .bold()
                     Spacer()
                     ZStack {
